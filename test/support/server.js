184407f3c30a9e20f1a0913a5ea1d90e57da100f
Irakli Nadareishvilii <inadarei@users.noreply.github.com>
2018-08-02 20:52:54 -0400

    change override implementation and add tests

--- test/support/server.js
+++ test/support/server.js
@@ -2,6 +2,7 @@ const log     = require('metalogger')();
 const express = require('express');
 const app = express();
 
+delete require.cache[require.resolve('../../lib/health')]; 
 const healthcheck = require("../../lib/health");
 
 healthcheck.addCheck('cassandra', 'timeout', async() => {
@@ -23,8 +24,11 @@ app.get('/', responder);
 app.get('/hello', responder);
 
 // Note: listening on port "0" results to listening on random, free port. Avoids conflicts.
-const server = app.listen(0, function () {
-  const port = server.address().port;
-  //log.info(`Test server listening at port ${port} \n`);
-});
-module.exports.server = server;
+module.exports.getServer = () => {
+    const server = app.listen(0, function () {
+        const port = server.address().port;
+        //log.info(`Test server listening at port ${port} \n`);
+    });
+
+    return server;
+};
