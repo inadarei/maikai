ed291796d1103a31720fa3dc194769cd9ce10f9e
Irakli Nadareishvilii <inadarei@users.noreply.github.com>
2018-08-05 19:08:58 -0400

    adding forgotten test for unknonw response status

---
+++ test/acceptance/express-unknown-response-status-test.js
@@ -0,0 +1,50 @@
+const test = require('blue-tape');
+const nf = require('node-fetch');
+
+test('Unknown Response Status From a Dependency', async t => {
+  const server = getServer();
+  try {
+    const util = require('../support/util');
+    const baseuri = util.serverUri(server);
+    const uri = `${baseuri}/health`;
+
+    const res = await nf(`${baseuri}/health`);  
+    t.equal(res.status, 503, 'correct http status code');   
+    const response = await res.json();
+
+    t.same(response.status, 'fail',
+      'correct status');
+
+    const regex = /unknown status:/;
+    const test = response.details['backend:something-malformed'].output;
+    t.same(Array.isArray(test.match(regex)), true, 'correct payload'); 
+
+  } catch (err) {
+    t.fail(err);
+  }
+  server.close();
+
+  //t.end();  // async tests dont need t.end() because blue-tape.
+});
+
+function getServer() {
+  const express = require('express');
+  const app = express();
+  const healthcheck  = require('../../lib/health')();
+  
+  healthcheck.addCheck('backend', 'something-malformed', async() => {
+    return {
+        status : 'nonexistent-status',
+        metricValue: 17,
+        metricUnit: "units"
+    };
+  });
+  app.use(healthcheck.handler());  
+
+  const server = app.listen(0, function () {
+    const port = server.address().port;
+    //log.info(`Test server listening at port ${port} \n`);
+  });
+
+  return server;
+}
