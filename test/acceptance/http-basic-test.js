04a9be66592334f33f3882b3c1a98b117f45f8e3
Irakli Nadareishvilii <inadarei@users.noreply.github.com>
2018-08-08 08:05:29 -0400

    added tests

---
+++ test/acceptance/http-basic-test.js
@@ -0,0 +1,61 @@
+const test = require('blue-tape');
+const nf = require('node-fetch');
+
+test('Basic Healthy No-Frameworks Health Check', async t => {
+
+  // avoid problems if this env var is already set from wherever test was run
+  process.env.NODE_HEALTH_ENDPOINT_PATH = "";
+
+  const server = getServer();
+  const util = require('../support/util');
+  const baseuri = util.serverUri(server);
+
+  try {
+    const res2 = await nf(`${baseuri}/health`);  
+    t.equal(res2.status, 200, 'proper http status code for /health');
+    t.equal(res2.headers.get('content-type'), 
+            'application/health+json; charset=utf-8',
+            'proper content type for health endpoint');
+    const response = await res2.json();
+    t.same(response.status, 'pass',
+      'healthcheck endpoint status works');
+    t.same(response.details["backend:http-downstream"].metricUnit, 'pureseconds',
+      'healthcheck endpoint details work');
+  
+  } catch (err) {
+    t.fail(err);
+  }
+
+  server.close();
+  
+});
+
+function getServer() {
+  const http = require('http');
+  const healthcheck = require('../../');
+  
+  const server = http.createServer( (request, response) => {
+    const check = healthcheck();
+
+    check.addCheck('backend', 'http-downstream', async() => {
+      return {
+          status : 'pass',
+          metricValue: 17,
+          metricUnit: "pureseconds"
+      };
+    });
+
+    const isHealthCheckCall = check.http(request, response);
+    if (isHealthCheckCall) return;
+  
+    response.end("HELLO! This is pretty amaziiiiing");
+  });
+
+  server.listen(0, function(err) {
+    if (err) console.error(err) && process.exit(1);
+    const port = server.address().port;
+    //console.log(`Test server listening at port ${port} \n`);
+  });
+
+  return server;
+}
