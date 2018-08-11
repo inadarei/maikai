c92f3adcf4296c6ec82778b4124be1a3b88da431
Irakli Nadareishvilii <inadarei@users.noreply.github.com>
2018-08-11 13:54:20 -0400

    test for promise support instead of async

--- test/acceptance/unit-test.js
+++ test/acceptance/unit-test.js
@@ -43,9 +43,14 @@ test('HealthCheck.addCheck()', async t => {
     const status = {
         status : 'pass',
     };
-    
     return fakepromise.promise(50, status);
   });
+  t.ok(true, "Calling addCheck() with an async function is fine");
+
+  check.addCheck('component-p', 'metric-p', 
+    fakepromise.promise(20, {status: 'pass'})
+  );
+  t.ok(true, "Calling addCheck() with an unresolved promise is also fine");
 
   const testName1 = 'Calling different addCheck() multiple times is fine';
   try {
@@ -63,7 +68,7 @@ test('HealthCheck.addCheck()', async t => {
   t.ok(true, testName1);
 
   const testName2 = 'Calling identical addCheck() multiple times not allowed';
-  const expectedErr = /Calling identical addCheck/i;
+  const expectedErr2 = /Calling identical addCheck/i;
   try {
     check.addCheck('component1', 'metric1', async () => {
       const status = {
@@ -74,7 +79,16 @@ test('HealthCheck.addCheck()', async t => {
     });
     t.fail(testName2);
   } catch (err) {
-    t.ok(expectedErr.test(err.toString()), testName2);
+    t.ok(expectedErr2.test(err.toString()), testName2);
+  }
+
+  const testName3 = 'Calling addCheck with callback that is neither async or a promise object errors-out';
+  const expectedErr3 = /.*Callback for .*? must be a promise or an async function/i;
+  try {
+    check.addCheck('component-obj', 'metric-obj', {something: 'else'});
+    t.fail(testName3);
+  } catch (err) {
+    t.ok(expectedErr3.test(err.toString()), testName3);
   }
   
 });
